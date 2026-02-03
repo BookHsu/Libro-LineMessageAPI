3795f613e903abe0305dd25ac817abc20251ac41
BookHsu <dsevenforbook@gmail.com>
2026-02-03 22:58:43 +0800

    Update CI and test targets

--- src/LineMessageApiSDK/Properties/AssemblyInfo.cs
+++ src/LineMessageApiSDK/Properties/AssemblyInfo.cs
@@ -1,5 +1,9 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("2.1.2.0")]
 [assembly: AssemblyFileVersion("2.1.2.0")]
 [assembly: AssemblyInformationalVersion("2.1.2")]
+
+// 允許測試專案存取 internal 類別
+[assembly: InternalsVisibleTo("LineMessageApiSDK.Tests")]
