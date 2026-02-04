b2d464d37586bf6e69cf5531690f28c771514f58
BookHsu-shinher <Book.Hsu@shin-her.com.tw>
2026-02-04 19:15:49 +0800

    Bump version to 2.1.3

--- src/LineMessageApiSDK.Extensions/Properties/AssemblyInfo.cs
+++ src/LineMessageApiSDK.Extensions/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
-[assembly: AssemblyInformationalVersion("2.1.2")]
+[assembly: AssemblyVersion("2.1.3.0")]
+[assembly: AssemblyFileVersion("2.1.3.0")]
+[assembly: AssemblyInformationalVersion("2.1.3")]
--- src/LineMessageApiSDK/Properties/AssemblyInfo.cs
+++ src/LineMessageApiSDK/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
-[assembly: AssemblyInformationalVersion("2.1.2")]
+[assembly: AssemblyVersion("2.1.3.0")]
+[assembly: AssemblyFileVersion("2.1.3.0")]
+[assembly: AssemblyInformationalVersion("2.1.3")]
 
 // 允許測試專案存取 internal 類別
 [assembly: InternalsVisibleTo("LineMessageApiSDK.Tests")]
